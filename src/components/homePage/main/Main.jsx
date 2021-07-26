05ed08f12683fcede870ce0bde77d84922a10b9a
gudbrant <61345734+gudbrant@users.noreply.github.com>
2021-07-26 12:51:42 +0800

    Create Main.jsx

---
+++ src/components/homePage/main/Main.jsx
@@ -0,0 +1,13 @@
+import { Fragment } from 'react'
+import Catalog from './Catalog'
+import TopSales from './TopSales'
+
+export default function Main() {
+    return (
+        <Fragment>
+            <TopSales />
+            <Catalog />
+        </Fragment>
+
+    )
+}
