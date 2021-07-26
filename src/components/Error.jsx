b54acd764795967c2e9fd60805c7ba0378e9d1b3
gudbrant <61345734+gudbrant@users.noreply.github.com>
2021-07-26 12:51:22 +0800

    Create Error.jsx

---
+++ src/components/Error.jsx
@@ -0,0 +1,13 @@
+import { Fragment } from 'react'
+export default function Error() {
+    return (
+        <Fragment>
+            <section class="top-sales">
+                <h2 class="text-center">Страница не найдена</h2>
+                <p>
+                    Извините, такая страница не найдена!
+                    </p>
+            </section>
+        </Fragment>
+    )
+}
